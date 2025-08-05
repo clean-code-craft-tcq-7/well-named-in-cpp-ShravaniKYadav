0b133275525e14d840c590c2d5222e32593ecb81
ShravaniKYadav <shravanikyadav123@gmail.com>
2025-08-05 12:02:04 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ namespace TelCoColorCoder
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-    const char* MajorColorNames[] = {
+    const char* MajorColorNames[] = 
+{
         "White", "Red", "Black", "Yellow", "Violet"
     };
     int numberOfMajorColors =
